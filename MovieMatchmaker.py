2e2693de4fff8489df96480b06aa860fab0cfc26
Anna Hudson <anna.hudson@ufl.edu>
2024-04-16 22:49:25 -0400

    Added a Rating class

--- MovieMatchmaker.py
+++ MovieMatchmaker.py
@@ -4,21 +4,28 @@ import itertools
 import threading
 import time
 import sys
+import Rating
+import Graph
 
 
 
 def main():
+    adj_list = Graph()
     rows = []
-
     with open("moviedata/Movie Data/ratings_export.csv", 'r') as file:
         reader = csv.reader(file)
         header = next(reader)
         for row in reader:
             rows.append(row)
+            rating = Rating(row[0], row[1], row[2], row[3])
+            adj_list.insertEdge(rating, rating)
+
 
 
     print(header)
     print(rows)
+
+
 
 
 
