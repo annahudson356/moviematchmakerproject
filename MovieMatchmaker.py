ece02a866701b7ce022ae11b601dc0dca76c2af0
chloe-bai <cbai71@yahoo.com>
2024-04-21 17:19:20 -0400

    movie, movie matchmaker (graph version) update: returning # of suggestions

--- MovieMatchmaker.py
+++ MovieMatchmaker.py
@@ -1,109 +1,53 @@
 import csv
-from datetime import datetime
 from Graph import Graph
-from Heap import Heap
+import Heap
 from Movie import Movie
 
 
 def main():
-    print("Welcome to Movie Matchmaker! Please follow the prompts below to be matched to your ideal movie!\n\n")
-    while True:
-        toExit = input("Press ENTER to continue, any other key to exit: ")
-        if toExit != "":
-            print("\n\n\nThanks for using Movie MatchMaker!")
-            print("--------------------\nCredits: \n")
-            print("Chloe Bai\nNora Choukri\nAnna Hudson")
-            break
+    # userSimilarMovie = input("Enter the movie you want to find something similar to ")
+    userFavoriteActors = input("Enter the actor you want to watch today ")
+    userFavoriteGenre = input("Enter your favorite genre ")
+    userPreferredLength = input("Input whether you want a short movie (<90), medium movie (90-120), long (120+) ")
+    howManySuggestions = int(input("Input how many suggestions you would like us to generate "))
 
-        userSimilarMovie = input("Enter the movie you want to find something similar to ")
-        userFavoriteActors = input("Enter one actor you want to watch today ")
-        userFavoriteGenre = input("Enter your favorite genre ")
-        userPreferredLength = input("Input whether you want a short movie (<90), medium movie (90-120), long (120+) ")
-        howManySuggestions = input("Input how many suggestions you would like us to generate ")
-
-        rows = []
-        with open("moviedata/movies.csv", 'r') as file:
-            reader = csv.reader(file)
-            header = next(reader)
-            try:
-                for row in reader:
-                    rows.append(row)
-            except UnicodeDecodeError:
-                pass
-
+    rows = []
+    with open("moviedata/movies.csv", 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader)
         try:
-            # Creates the graph and the heap data structure
-            graph = populateGraph(rows)
-            movieHeap = populateHeap(rows)
-
-
-            # Does the Matchmaking process
-            matchGraph(graph, userSimilarMovie, userFavoriteGenre, userFavoriteActors, userPreferredLength)
-            matchHeap(movieHeap, userSimilarMovie, userFavoriteGenre, userFavoriteActors, userPreferredLength, howManySuggestions)
-
-
-            print("Data Collection: Was the Heap faster than the Graph?")
-            print(matchmaker(graph, userSimilarMovie, userFavoriteGenre, userFavoriteActors, userPreferredLength) >
-            matchHeap(movieHeap, userSimilarMovie, userFavoriteGenre, userFavoriteActors, userPreferredLength))
-        except ValueError:
-            print("Invalid Input! Please Try Again!\n\n\n")
-            continue
+            for row in reader:
+                rows.append(row)
+        except UnicodeDecodeError:
+            pass
+    graph = populateGraph(rows)
+    matchmaker(graph, userFavoriteGenre, userFavoriteActors, userPreferredLength, howManySuggestions)
 
 
 def populateGraph(rows):
-    time_a = datetime.now()
     graph = Graph()
     for i in range(len(rows)):  # for all rows of movies
-        if rows[i][14] == "":
-            movie = Movie(rows[i][0], rows[i][2], rows[i][9], -1, rows[i][5])
-        else:
-            movie = Movie(rows[i][0], rows[i][2], rows[i][9], float(rows[i][14]), rows[i][5])
+        movie = Movie(rows[i][0], rows[i][2], rows[i][9], float(rows[i][14]), rows[i][5])
         graph.addVertex(movie)
-    time_b = datetime.now()
-    print("Time taken to build the graph: " + str(time_b - time_a))
     return graph
 
-def populateHeap(rows):
-    time_a = datetime.now()
-    heap = Heap()
-    for i in range(len(rows)):
-        if rows[i][14] == "":
-            movie = Movie(rows[i][0], rows[i][2], rows[i][9], -1, rows[i][5])
-        else:
-            movie = Movie(rows[i][0], rows[i][2], rows[i][9], float(rows[i][14]), rows[i][5])
-        heap.getArr().append(movie)
-    heap.heapifyDown(0)
-    time_b = datetime.now()
-    print("Time taken to find your ideal movie using the heap: " + str(time_b - time_a))
-    return heap
 
-
-
-def matchGraph(graph, userSimilarMovie, userFavoriteGenre, userFavoriteActors, userPreferredLength):
-    time_a = datetime.now()
+def matchmaker(graph, userFavoriteGenre, userFavoriteActors, userPreferredLength, howManySuggestions):
     idealMovie = Movie("", userFavoriteGenre, userFavoriteActors, userPreferredLength, 10)
     graph.addVertex(idealMovie)
     movies = graph.getEdges(idealMovie)
-    top = idealMovie.movie
-    highestSim = 0
-    for movie in movies:
-        if movie.getSimilarity(idealMovie) > highestSim:
-            if movie.movie != idealMovie.movie:
-                highestSim = movie.getSimilarity(idealMovie)
-                top = movie.movie
-    print(top)
-    time_b = datetime.now()
-    print("Time taken to find your ideal movie using the heap: " + str(time_b - time_a))
-    return time_b - time_a
-
-
-def matchHeap(heap, userSimilarMovie, userFavoriteGenre, userFavoriteActors, userPreferredLength, howManySuggestions):
-    time_a = datetime.now()
-    heap.kthLargestElements(howManySuggestions)
-    time_b = datetime.now()
-    print("Time taken to find your ideal movie using the heap: " + str(time_b - time_a))
-    return time_b - time_a
-
+    movie_list = []
+    while len(movie_list) < howManySuggestions:
+        top = ""
+        highestSim = 0
+        for movie in movies:
+            if movie.getSimilarity(idealMovie) > highestSim:
+                if movie.movie not in movie_list and movie.movie != idealMovie.movie:
+                    highestSim = movie.getSimilarity(idealMovie)
+                    top = movie.movie
+        movie_list.append(top)
+    for movie in movie_list:
+        print(movie)
 
 
 if __name__ == '__main__':
